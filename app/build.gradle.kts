8cdd509b11b83dfe4cc0560ea5194239a4ce8b32
https-michi <glitchmichi@gmail.com>
2025-11-03 02:15:10 -0500

    refactor(ui): modificar vista base de perfil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     //
     implementation(libs.androidx.navigation.compose)
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
     //
     testImplementation(libs.junit)
